8c6298ba521376d658d98a255e5e8f252a966506
dpqkdqkj <105240473+dpqkdqkj@users.noreply.github.com>
2023-07-17 04:21:22 +0300

    Create conf.d

---
+++ fonts/conf.d
@@ -0,0 +1,8 @@
+10-hinting-slight.conf     42-luxi-mono.conf  60-latin.conf
+10-sub-pixel-none.conf     45-generic.conf    65-fonts-persian.conf
+10-yes-antialias.conf      45-latin.conf      65-nonlatin.conf
+11-lcdfilter-default.conf  48-spacing.conf    69-unifont.conf
+20-no-embedded.conf        49-sansserif.conf  70-no-bitmaps.conf
+20-unhint-small-vera.conf  50-user.conf       80-delicious.conf
+30-metric-aliases.conf     51-local.conf      90-synthetic.conf
+40-nonlatin.conf           60-generic.conf    README
